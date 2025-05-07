f2fddc2ddf3231f262b3f287ee3e1a5aec0dfff3
gupadovezi <padovezigustavo@gmail.com>
2025-05-06 22:15:56 -0300

    Add files via upload

---
+++ Fluxo_básico_trabalho.R
@@ -0,0 +1,12 @@
+> 1/200*30
+[1] 0.15
+> x<-3*4
+> x<-3*4
+> seq(1,10)
+[1]  1  2  3  4  5  6  7  8  9 10
+> x<-"hello world"
+> y<-seq(1,10,lenght.out=5)
+Warning message:
+  In seq.default(1, 10, lenght.out = 5) :
+  extra argument ‘lenght.out’ will be disregarded 
+> y<-seq(1,10,length.out=5)
